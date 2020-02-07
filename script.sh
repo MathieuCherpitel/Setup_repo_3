f36bd343802654d00a3bfe2849154c4f6e733f20
Mathieu <mathieu.cherpitel@epitech.eu>
2020-02-07 16:14:32 +0100

    [+] cleaned code better structure, basic security managment preparing the super quick setup option

--- script.sh
+++ script.sh
@@ -8,6 +8,7 @@
 
 #   this scripts handle help flag such as :
 #   Help         :  -h
+#   Setup help   :  -s
 #
 #   when started, the script will ask you what type of setup you want
 #
@@ -22,8 +23,25 @@
 #                   - no
 #
 #   Language:       -c
+#                   -c++
 #                   -python
 
+#####################################
+#            DOCUMENTATION          #
+#####################################
+#   prints the security infos
+print_security_info()
+{
+    echo "SECURITY"
+    echo "Curently in this version, the EPITECH password of the user is stocked in a file called .info"
+    echo "this file is protected by the super user rights but in order to protect it and to read it, the script must ask you for your root password."
+    echo "This means that if you don't restart your terminal, your EPITECH password is vulnerable for 5 minutes (effect time of super user)."
+    echo "Also the only easier way to get your password is to find your root password, make sure to have a safe one"
+    echo "I will be working on an encryption method for the next updates, make sure to check the github of the project if you find any interesting security issue/ideas."
+    exit
+}
+
+#   print infos (-h flag)
 check_help() {
     input=$1
     if [ "$1" == "-h" ]; then
@@ -47,44 +65,15 @@ check_help() {
         echo "  -simple (will create a c classic Makefile"
         exit
     fi
-}
-
-#   if the user puts as arguments sq repo_name we setup a "classic c repo"
-super_quick_setup()
-{
-    if [ -z $2 ]
+    if [ "$1" == "-s" ]
     then
-        echo "missing repo name !"
-        exit 0
+        print_security_info
     fi
-
-    #creates the repo gives access rights and clone 
-    blih repository create $2
-    blih repository setacl $2 ramassage-tek r
-    blih repository getacl $2
-    git clone git@git.epitech.eu:/mathieu.cherpitel@epitech.eu/$2 && echo repository created and cloned
-    cp -r ~/.setup_repo/lib/* $2 && echo lib copied
-    cd $2
 }
 
-complete_setup()
-{
-    echo "complete setup"
-}
-
-simple_setup()
-{
-    #   get login
-    cd ~/.setup_repo_v3
-    login="$(cat info)"
-    openssl rsautl -inkey ~/.setup_repo_v3/key.txt -decrypt <~/.setup_repo_v3/password.bin > soluce
-    password="$(cat soluce)"
-    rm soluce
-    cd -
-    echo "login : $login"
-    echo "password : $password"
-}
-
+#####################################
+#         FIRST INITIALISATION      #
+#####################################
 #   gets the setup option and calls either simple_setup or complete_setup
 get_setup_option()
 {
@@ -102,6 +91,43 @@ get_setup_option()
     fi
 }
 
+#   gets the user infos and remove acces rights to non root users
+init_setup()
+{
+    cd ~/.setup_repo_v3
+    echo "Setup_repo v3 does not seems to be set up for more info use --info"
+    echo "Enter your Epitech adress :"
+    read login
+    if [ $login == "--info" ]
+    then
+        print_security_info
+        init_setup
+    fi
+    touch info
+    #   we are putting the user infos in a file to ask them only once
+    echo "$login" >> info
+    echo "Enter your Password"
+    read -s password
+    if [ $login == "--info" ]
+    then
+        print_security_info
+        init_setup
+    fi
+    echo "$password" >> info
+
+    ##  It would be nice to crypt the password here before putting it in a file
+
+    #   for secutiry reasons, we remove access rights to non root users to prevent then from reading the file
+    #   the user must make sure that his root password is strong enought to protect his EPITECH login
+    #   we also hide this file
+    chmod o-r info
+    chmod u-r info
+    chmod g-r info
+    mv info .info
+    echo "Your setup my repo is now set-up"
+}
+
+#   checks the mandatories files
 check_files()
 {
     if [ ! -d ~/.setup_repo_v3 ]
@@ -114,28 +140,43 @@ check_files()
         echo "missing lib directory in ~/.setup_repo try to create one and see -s for more info"
         exit
     fi
-    if [ ! -f ~/.setup_repo_v3/info ]
+    if [ ! -f ~/.setup_repo_v3/.info ]
     then
-        echo "Setup_repo v3 does not seems to be set up, if it's not true make sure to report the bug via github"
-        echo "Enter your Epitech adress :"
-        read login
-        cd ~/.setup_repo_v3
-        echo "$login" >> info
-        echo "generating key"
-        openssl genrsa -out ~/.setup_repo_v3/key.txt 2048
-        echo "Enter your Epitech password (it will be encoded in key.txt file and will not be printed at any time)"
-        read password 
-        echo "$password" | openssl rsautl -inkey ~/.setup_repo_v3/key.txt -encrypt >~/.setup_repo_v3/password.bin
+        init_setup
     fi
 }
 
-###########################################
-#   this is the main body of the script   #
-###########################################
-check_files
+#####################################
+#         SUPER QUICK SETUP         #
+#####################################
+super_quick_setup()
+{
+    mail=sudo sed -n 1p ~/.setup_repo_v3/.info
+    password=sudo sed -n 2p ~/.setup_repo_v3/.info
+    echo $mail
+    echo $password
+}
+
+#####################################
+#             MAIN BODY             #
+#####################################
+
+#   log as root user
+#   this is necessary to read the .info protected file
+#   keep in mind that if you do not restart your terminal, your password is vulnerable for 5 minutes (see sudo use and print security info)
+sudo sleep 0
+
+#   Init
+#   flags possibles : -h -s
 check_help $1
+#   checking the mandatory files and if needed creates the info file
+check_files
+
+#   Check super quick setup option
 if [ "$1" == "sq" ]
 then
-    super_quick_setup $1 $2
+    super_quick_setup
+    exit
 fi
+#   Check flags
 get_setup_option
